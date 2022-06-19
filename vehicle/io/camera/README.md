1ddb199af4bf604882ce0d0d935b71c3991c8052
Andrea F. Daniele <afdaniele@ttic.edu>
2022-06-19 11:38:59 -0500

    added examole vehicle/io/camera

---
+++ vehicle/io/camera/README.md
@@ -0,0 +1,42 @@
+# I/O: Camera
+
+## Introduction
+
+This demo is designed to work properly with the map `loop_0` you find 
+in this repository.
+Using this demo with a different map might not produce the 
+expected result as the requested vehicle might not be present
+in the new map.
+
+
+## Step 1: Build the demo
+
+Run the following command from inside this directory to build the demo.
+
+```shell
+dts devel build
+```
+
+## Step 2: Run the duckiematrix
+
+Run the following command from inside the directory `maps/` you can 
+find at the root of this repository to launch the duckiematrix on the
+map `loop_0`.
+
+```shell
+dts matrix run --standalone --map ./loop_0
+```
+
+You should see a duckiematrix renderer pop up.
+
+
+## Step 3: Run the demo
+
+Run the following command from inside this directory to run the demo.
+
+```shell
+dts devel run -X
+```
+
+You should see a matplotlib window pop up rendering the image captured 
+by the vehicle's camera in the duckiematrix.
---
+++ vehicle/io/camera/packages/example_io_camera/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import time
+from collections import defaultdict
+from queue import Queue, Empty
+from threading import Thread
+from typing import Any, Tuple, Dict, List
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from turbojpeg import TurboJPEG
+
+from dt_duckiematrix_protocols import Matrix
+
+
+# vehicle calibration
+# - camera
+camera_info: Dict[str, int] = {
+    "width": 640,
+    "height": 480
+}
+
+# JPEG decoder
+jpeg = TurboJPEG()
+
+# create matplot window
+window = plt.imshow(np.zeros((camera_info["height"], camera_info["width"], 3)))
+plt.axis("off")
+fig = plt.figure(1)
+plt.subplots_adjust(left=0.0, right=1.0, top=1.0, bottom=0.0)
+plt.pause(0.01)
+
+
+class CameraIO:
+
+    def __init__(self):
+        # create connection to the matrix engine
+        matrix = Matrix("localhost")
+        # create connection to the vehicle
+        self.robot = matrix.robots.DB21M("map_0/vehicle_0")
+
+    def run(self):
+        while not self.is_shutdown:
+            cframe = self.robot.camera.capture(block=True)
+            # get frame as uint8 array
+            jpg = cframe.as_uint8()
+            bgr = jpeg.decode(jpg)
+            # bgr -> rgb
+            rgb = bgr[:, :, [2, 1, 0]]
+            # show frame
+            window.set_data(rgb)
+            fig.canvas.draw_idle()
+            fig.canvas.start_event_loop(0.001)
+
+    @property
+    def is_shutdown(self):
+        # TODO: link this to SIGINT
+        return False
+
+
+if __name__ == "__main__":
+    node = CameraIO()
+    node.run()
