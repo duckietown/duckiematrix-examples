d89ff02121b38854c4f41774525ac90ce80b21b5
Liam Paull <paulll@iro.umontreal.ca>
2025-03-29 13:57:12 -0400

    remove printouts

--- vehicle/demos/gym_protocol/packages/example_gym_protocol/main.py
+++ vehicle/demos/gym_protocol/packages/example_gym_protocol/main.py
@@ -7,8 +7,5 @@ obs, info = env.reset()
 
 for i in range(100):
     action = env.action_space.sample()
-    print(f"sending action {action}")
     obs, reward, truncated, terminated, info = env.step(action)
-    print("received observation")
     sleep(0.1)
-    #env.render()
